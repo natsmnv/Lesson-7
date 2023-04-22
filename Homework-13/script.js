87303a07c6c93c182f7339883ebe2da3c40c5453
natsmnv <natnatanatasha@gmail.com>
2023-04-22 21:43:50 +0300

    Add index and script files

--- Homework-13/script.js
+++ Homework-13/script.js
@@ -8,28 +8,6 @@
 // const key = generateKey(16, characters);
 // console.log(key); // eg599gb60q926j8i
 
-// const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-// function generateKey(length, characters) {
-//     let str = '';
-//     for (let i = 0; i < length; i++) {
-//         let pos = Math.floor(Math.random() * length);
-//         str += characters.substring(pos, pos+1);
-//     }
-//     return str;
-// }
-
-// const key = generateKey(16, characters);
-// console.log(key); // eg599gb60q926j8i
-
-
-// let rand = Math.random();
-// console.log(rand);
-// console.log(rand.toString(36));
-// console.log(rand.toString(12));
-// console.log(rand.toString(2));
-
-
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
 function generateKey(length, characters) {
